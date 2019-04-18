b4f1f8bd51763787816b0f7a0c96c9275a952db0
srisainaveen <srisainaveen.burugupalli@iipay.com>
2019-04-18 08:45:36 +0100

    1804

--- gpmsMain/src/test/java/gpmsAutomation/gpmsMain/addingNewDetails.java
+++ gpmsMain/src/test/java/gpmsAutomation/gpmsMain/addingNewDetails.java
@@ -28,7 +28,8 @@ import gpmsAutomation.testInputs.testInputGPMS;
 
 public class addingNewDetails extends basicDetails {
 
-	@Test //Adding New employee to TST3 for given details in testInputGPMS class
+	@Test 
+	//Adding New employee to TST3 for given details in testInputGPMS class
 	public void addEmployee() throws AWTException, InterruptedException, IOException {
 		
 		driver.findElement(By.xpath(menuPageObjects.jumpToTextBox)).sendKeys(testInputGPMS.emplyeeNo);
@@ -76,6 +77,107 @@ public class addingNewDetails extends basicDetails {
 	}
 	
 	@Test
+	//Adding new company
+	public void addNewCompany() throws IOException, AWTException, InterruptedException {
+		
+		menuBarLinks.goToCompanyAdmin(driver);
+		
+		if(driver.findElements(By.xpath(companyAdminPageObjects.newCompany)).size()==0) {
+			System.out.println("Failed adding new Company, as we could not able to navigate to Company Selection Page");
+			commonMethods.takeScreenShot(driver, "Failed adding new Company_Navigation error");
+			Assert.fail("Failed adding new Company, as we could able to navigate to Company Selection Page");
+		}else {
+			driver.findElement(By.xpath(companyAdminPageObjects.newCompany)).click();
+			driver.findElement(By.xpath(companyAdminPageObjects.companyName)).sendKeys(testInputGPMS.newCompanyName);
+			driver.findElement(By.xpath(companyAdminPageObjects.addressLine1)).sendKeys(testInputGPMS.companyAddressLine1);
+			driver.findElement(By.xpath(companyAdminPageObjects.postalTown)).sendKeys(testInputGPMS.companyPostalTown);
+			driver.findElement(By.xpath(companyAdminPageObjects.country)).sendKeys(testInputGPMS.companyCountry);
+			
+			driver.findElement(By.xpath(companyAdminPageObjects.save)).click();
+			if(driver.findElement(By.xpath(companyAdminPageObjects.success)).getAttribute("innerText").contains("Changes successfully saved")) {
+				System.out.println("Passed adding new Company Details");
+				commonMethods.takeScreenShot(driver, "Passed adding new Company Details");
+				driver.findElement(By.xpath(companyAdminPageObjects.returnToCompanySelection)).click();
+			}else {
+				System.out.println("Failed adding new Company Details");
+				if(driver.findElement(By.xpath(companyAdminPageObjects.error)).getAttribute("innerText").contains("ERROR")) {
+				System.out.println(driver.findElement(By.xpath(companyAdminPageObjects.error)).getAttribute("innerText"));}
+				System.out.println(driver.findElement(By.xpath(companyAdminPageObjects.errorsummary)).getAttribute("innerText"));
+				commonMethods.takeScreenShot(driver, "Failed adding new Company Details");
+				Assert.fail("Failed adding new Company Details");
+			}
+		}
+	}
+	
+	@Test
+	//Adding new Org Unit
+	public void addNewOrgUnit() throws IOException, AWTException, InterruptedException {
+		
+		menuBarLinks.goToUserDefinedOrgUnits(driver);
+		
+		int i=driver.findElements(By.xpath(userDefinedOrgUnitAdminPageObjects.userDefinedOrgsTable+"/tbody/tr")).size();
+		Actions action = new Actions(driver);
+		WebElement mainMenu = driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.userDefinedOrgsTable+"/tbody/tr["+i+"]"));
+		action.moveToElement(mainMenu).click().sendKeys(testInputGPMS.newOrgUnit).perform();
+		driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.save)).click();
+		
+		if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.sucessful)).getAttribute("innerText").contains("successfully")) {
+			commonMethods.takeScreenShot(driver, "Passed Saving New Org unit");
+			System.out.println("Passed: New Org unit saved successfully");
+		}else {
+			if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.error)).getAttribute("innerText").contains("ERROR")) {
+				System.out.println(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.error)).getAttribute("innerText"));}
+			if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.warning)).getAttribute("innerText").contains("Warning")) {
+				System.out.println(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.warning)).getAttribute("innerText"));}
+			commonMethods.takeScreenShot(driver, "Failed Saving New Org unit");
+			Assert.fail("Failed: Saving New Org unit");
+		}
+	
+	//Adding details for new Org unit created	
+		userDefinedOrgUnitAdminPageObjects.newUserOrgUnitConfiguration(driver, testInputGPMS.newOrgUnit, testInputGPMS.newNames, testInputGPMS.newReferences);
+	}
+	
+	@Test
+	//Adding new Element Type
+	public void addNewElementType() throws IOException, AWTException, InterruptedException {
+		menuBarLinks.goToElementTypeAdmin(driver);
+		driver.findElement(By.xpath(elementTypeSelectionPageObjects.addNewElement)).click();
+		driver.findElement(By.xpath(elementTypeSelectionPageObjects.nameElementType)).sendKeys(testInputGPMS.newElementTypeName);
+		List <WebElement> fromParties=driver.findElements(By.xpath(elementTypeSelectionPageObjects.fromParty+"/option"));
+		for(WebElement option :fromParties) {
+			if (option.getText().contains(testInputGPMS.newElementTypeFromParty)) {
+	        option.click();
+	        break;
+			}
+		}
+		List <WebElement> toParties=driver.findElements(By.xpath(elementTypeSelectionPageObjects.toParty+"/option"));
+		for(WebElement option :toParties) {
+			if (option.getText().contains(testInputGPMS.newElementTypeToParty)) {
+	        option.click();
+	        break;
+			}
+		}
+		if(testInputGPMS.newElementTypeDescription!=null) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.descriptionElementType)).sendKeys(testInputGPMS.newElementTypeDescription);}
+		if(testInputGPMS.newElementTypeConsolidation!=null) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.consolidationETypeName)).sendKeys(testInputGPMS.newElementTypeConsolidation);}
+		if(testInputGPMS.newElementTypeNegate==true) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.negateCheckBox)).click();}
+		if(testInputGPMS.newElementTypeObsolete==true) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.obsoleteCheckBox)).click();}
+		driver.findElement(By.xpath(elementTypeSelectionPageObjects.save)).click();
+		
+		if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.sucessful)).getAttribute("innerText").contains("successfully")) {
+			commonMethods.takeScreenShot(driver, "Passed Saving New Element Type");
+			System.out.println("Passed: New Element Type saved successfully");
+		}else {
+			System.out.println("Failed: Adding new Element Type");
+			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.error)).getAttribute("innerText").contains(" ")){System.out.println("	Error Message: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.error)).getAttribute("innerText"));}
+			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.warning)).getAttribute("innerText").contains(" ")){System.out.println("	Warning Message: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.warning)).getAttribute("innerText"));}
+			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.errorSummary)).getAttribute("innerText").contains(" ")){System.out.println("	Error Summary: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.errorSummary)).getAttribute("innerText"));}
+			commonMethods.takeScreenShot(driver, "Failed Saving New Element Type");
+			Assert.fail("Failed: Saving New Element Type");
+		}
+	}
+
+	@Test
+	//Adding new Payroll
 	public void addPayroll() throws AWTException, InterruptedException, IOException {
 		
 		menuBarLinks.goToForPayroll(driver);
@@ -144,6 +246,7 @@ public class addingNewDetails extends basicDetails {
 	}
 
 	@Test
+	//Editing/Adding pay periods for payroll
 	public void editPayrollPeriods() throws AWTException, InterruptedException, IOException {
 		
 		menuBarLinks.goToForPayroll(driver);
@@ -165,7 +268,8 @@ public class addingNewDetails extends basicDetails {
 		}
 	}
 	
-	@Test //To Create Payroll Assignment for existing employee
+	@Test 
+	//To Create Payroll Assignment for existing employee
 	public void createPayrollAssignment() throws IOException, AWTException, InterruptedException {
 		
 		driver.findElement(By.xpath(menuPageObjects.jumpToTextBox)).sendKeys(testInputGPMS.emplyeeNo);
@@ -241,102 +345,82 @@ public class addingNewDetails extends basicDetails {
 		}
 	}
 
-	@Test
-	public void addNewCompany() throws IOException, AWTException, InterruptedException {
+	@Test 
+	//To add PayDeds and Entitlements
+	public void addPayDedsEntitlements() throws IOException, AWTException, InterruptedException {
 		
-		menuBarLinks.goToCompanyAdmin(driver);
+		driver.findElement(By.xpath(menuPageObjects.jumpToTextBox)).sendKeys(testInputGPMS.emplyeeNo);
+		menuBarLinks.goToJumpToEmployeeNumber(driver);
+		int warning = driver.findElements(By.xpath(menuPageObjects.warning)).size();
 		
-		if(driver.findElements(By.xpath(companyAdminPageObjects.newCompany)).size()==0) {
-			System.out.println("Failed adding new Company, as we could not able to navigate to Company Selection Page");
-			commonMethods.takeScreenShot(driver, "Failed adding new Company_Navigation error");
-			Assert.fail("Failed adding new Company, as we could able to navigate to Company Selection Page");
+		if(warning==1) {
+			System.out.println("Failed: Cant create Payroll Assignement, as  Employee Number'"+ testInputGPMS.emplyeeNo +"' don't exists");
+			commonMethods.takeScreenShot(driver, "Failed creating Payroll Assignment_Emp No dont exists");
+			Assert.fail("Failed: Cant create Payroll Assignement, as  Employee Number'"+ testInputGPMS.emplyeeNo +"' don't exists");
 		}else {
-			driver.findElement(By.xpath(companyAdminPageObjects.newCompany)).click();
-			driver.findElement(By.xpath(companyAdminPageObjects.companyName)).sendKeys(testInputGPMS.newCompanyName);
-			driver.findElement(By.xpath(companyAdminPageObjects.addressLine1)).sendKeys(testInputGPMS.companyAddressLine1);
-			driver.findElement(By.xpath(companyAdminPageObjects.postalTown)).sendKeys(testInputGPMS.companyPostalTown);
-			driver.findElement(By.xpath(companyAdminPageObjects.country)).sendKeys(testInputGPMS.companyCountry);
 			
-			driver.findElement(By.xpath(companyAdminPageObjects.save)).click();
-			if(driver.findElement(By.xpath(companyAdminPageObjects.success)).getAttribute("innerText").contains("Changes successfully saved")) {
-				System.out.println("Passed adding new Company Details");
-				commonMethods.takeScreenShot(driver, "Passed adding new Company Details");
-				driver.findElement(By.xpath(companyAdminPageObjects.returnToCompanySelection)).click();
+			driver.findElement(By.xpath(employeeDetailsPageObjects.createPayrollAssignment)).click();
+			
+			List <WebElement> payrolls=driver.findElements(By.xpath(createPayrollAssignmentPageObjects.payroll+"/option"));
+			for(WebElement option :payrolls) {
+				if (option.getText().contains(testInputGPMS.payrollName)) {
+		        option.click();
+		        break;
+				}
+			}
+			Thread.sleep(500);
+			if(driver.findElements(By.xpath(createPayrollAssignmentPageObjects.paymentType)).size()==0) {
+				System.out.println("Cant create Payroll Assignement, as  Payroll Name'"+ testInputGPMS.payrollName +"' don't exists");
+				commonMethods.takeScreenShot(driver, "Failed creating Payroll Assignment_Payroll Name dont exists");
+				Assert.fail("Cant create Payroll Assignement, as  Payroll Name'"+ testInputGPMS.payrollName +"' don't exists");
+			}	
+			driver.findElement(By.xpath(createPayrollAssignmentPageObjects.payrollStartDate)).sendKeys(testInputGPMS.startDate);
+			Thread.sleep(500);
+			driver.findElement(By.xpath(createPayrollAssignmentPageObjects.actualStartDate)).sendKeys(testInputGPMS.startDate);
+			Thread.sleep(500);
+			List <WebElement> employeeAddresses=driver.findElements(By.xpath(createPayrollAssignmentPageObjects.employeeAddress+"/option"));
+			for(WebElement option :employeeAddresses) {
+				if (option.getText().contains(testInputGPMS.addressRef)) {
+		        option.click();
+		        break;
+				}
+			}
+			Thread.sleep(500);
+			
+			List <WebElement> paymentTypes=driver.findElements(By.xpath(createPayrollAssignmentPageObjects.paymentType+"/option"));
+			for(WebElement option :paymentTypes) {
+				if (option.getText().contains(testInputGPMS.paymentType)) {
+		        option.click();
+		        break;
+				}
+			}
+			Thread.sleep(500);
+			List <WebElement> splitTypes=driver.findElements(By.xpath(createPayrollAssignmentPageObjects.splitType+"/option"));
+			for(WebElement option :splitTypes) {
+				if (option.getText().contains(testInputGPMS.splitType)) {
+		        option.click();
+		        break;
+				}
+			}
+			Thread.sleep(500);
+			driver.findElement(By.xpath(createPayrollAssignmentPageObjects.save)).click();	
+			Thread.sleep(500);
+			
+			if(driver.findElements(By.xpath(createPayrollAssignmentPageObjects.employeePayrollDetails)).size()==0) {
+				System.out.println("Failed Create Payroll Assignement step");
+				if(driver.findElement(By.xpath(createPayrollAssignmentPageObjects.errorMessage1)).isDisplayed()==true) System.out.println(driver.findElement(By.xpath(createPayrollAssignmentPageObjects.errorMessage1)).getAttribute("innerText"));
+				if(driver.findElement(By.xpath(createPayrollAssignmentPageObjects.errorMessage2)).isDisplayed()==true) System.out.println(driver.findElement(By.xpath(createPayrollAssignmentPageObjects.errorMessage2)).getAttribute("innerText"));
+				commonMethods.takeScreenShot(driver, "Failed Create Payroll Assignement step");
+				Assert.fail("Failed Create Payroll Assignement step");
 			}else {
-				System.out.println("Failed adding new Company Details");
-				if(driver.findElement(By.xpath(companyAdminPageObjects.error)).getAttribute("innerText").contains("ERROR")) {
-				System.out.println(driver.findElement(By.xpath(companyAdminPageObjects.error)).getAttribute("innerText"));}
-				System.out.println(driver.findElement(By.xpath(companyAdminPageObjects.errorsummary)).getAttribute("innerText"));
-				commonMethods.takeScreenShot(driver, "Failed adding new Company Details");
-				Assert.fail("Failed adding new Company Details");
+				System.out.println("Passed: Create Payroll Assignement Successful");
+				commonMethods.takeScreenShot(driver, "Create Payroll Assignement Successful");
 			}
+			
+			driver.findElement(By.xpath(createPayrollAssignmentPageObjects.backToEmployeeDetails)).click();
+			
 		}
 	}
 	
-	@Test
-	public void addNewOrgUnit() throws IOException, AWTException, InterruptedException {
-		
-		menuBarLinks.goToUserDefinedOrgUnits(driver);
-	
-	//Adding new Org Unit	
-		int i=driver.findElements(By.xpath(userDefinedOrgUnitAdminPageObjects.userDefinedOrgsTable+"/tbody/tr")).size();
-		Actions action = new Actions(driver);
-		WebElement mainMenu = driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.userDefinedOrgsTable+"/tbody/tr["+i+"]"));
-		action.moveToElement(mainMenu).click().sendKeys(testInputGPMS.newOrgUnit).perform();
-		driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.save)).click();
-		
-		if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.sucessful)).getAttribute("innerText").contains("successfully")) {
-			commonMethods.takeScreenShot(driver, "Passed Saving New Org unit");
-			System.out.println("Passed: New Org unit saved successfully");
-		}else {
-			if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.error)).getAttribute("innerText").contains("ERROR")) {
-				System.out.println(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.error)).getAttribute("innerText"));}
-			if(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.warning)).getAttribute("innerText").contains("Warning")) {
-				System.out.println(driver.findElement(By.xpath(userDefinedOrgUnitAdminPageObjects.warning)).getAttribute("innerText"));}
-			commonMethods.takeScreenShot(driver, "Failed Saving New Org unit");
-			Assert.fail("Failed: Saving New Org unit");
-		}
-	
-	//Adding details for new Org unit created	
-		userDefinedOrgUnitAdminPageObjects.newUserOrgUnitConfiguration(driver, testInputGPMS.newOrgUnit, testInputGPMS.newNames, testInputGPMS.newReferences);
-	}
-	
-	
-	@Test
-	public void addNewElementType() throws IOException, AWTException, InterruptedException {
-		menuBarLinks.goToElementTypeAdmin(driver);
-		driver.findElement(By.xpath(elementTypeSelectionPageObjects.addNewElement)).click();
-		driver.findElement(By.xpath(elementTypeSelectionPageObjects.nameElementType)).sendKeys(testInputGPMS.newElementTypeName);
-		List <WebElement> fromParties=driver.findElements(By.xpath(elementTypeSelectionPageObjects.fromParty+"/option"));
-		for(WebElement option :fromParties) {
-			if (option.getText().contains(testInputGPMS.newElementTypeFromParty)) {
-	        option.click();
-	        break;
-			}
-		}
-		List <WebElement> toParties=driver.findElements(By.xpath(elementTypeSelectionPageObjects.toParty+"/option"));
-		for(WebElement option :toParties) {
-			if (option.getText().contains(testInputGPMS.newElementTypeToParty)) {
-	        option.click();
-	        break;
-			}
-		}
-		if(testInputGPMS.newElementTypeDescription!=null) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.descriptionElementType)).sendKeys(testInputGPMS.newElementTypeDescription);}
-		if(testInputGPMS.newElementTypeConsolidation!=null) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.consolidationETypeName)).sendKeys(testInputGPMS.newElementTypeConsolidation);}
-		if(testInputGPMS.newElementTypeNegate==true) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.negateCheckBox)).click();}
-		if(testInputGPMS.newElementTypeObsolete==true) {driver.findElement(By.xpath(elementTypeSelectionPageObjects.obsoleteCheckBox)).click();}
-		driver.findElement(By.xpath(elementTypeSelectionPageObjects.save)).click();
-		
-		if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.sucessful)).getAttribute("innerText").contains("successfully")) {
-			commonMethods.takeScreenShot(driver, "Passed Saving New Element Type");
-			System.out.println("Passed: New Element Type saved successfully");
-		}else {
-			System.out.println("Failed: Adding new Element Type");
-			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.error)).getAttribute("innerText").contains(" ")){System.out.println("	Error Message: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.error)).getAttribute("innerText"));}
-			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.warning)).getAttribute("innerText").contains(" ")){System.out.println("	Warning Message: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.warning)).getAttribute("innerText"));}
-			if(driver.findElement(By.xpath(elementTypeSelectionPageObjects.errorSummary)).getAttribute("innerText").contains(" ")){System.out.println("	Error Summary: "+driver.findElement(By.xpath(elementTypeSelectionPageObjects.errorSummary)).getAttribute("innerText"));}
-			commonMethods.takeScreenShot(driver, "Failed Saving New Element Type");
-			Assert.fail("Failed: Saving New Element Type");
-		}
-	}
+
 }
